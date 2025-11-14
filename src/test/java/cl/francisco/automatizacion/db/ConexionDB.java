db2ea70165840bfdf38325e156a12798052652ac
Frxnciscx20 <panchomanuek@gmail.com>
2025-11-13 21:58:14 -0300

    se crea yml para ejecutar github

--- src/test/java/cl/francisco/automatizacion/db/ConexionDB.java
+++ src/test/java/cl/francisco/automatizacion/db/ConexionDB.java
@@ -8,7 +8,7 @@ public class ConexionDB {
 
     private static final String URL = "jdbc:postgresql://aws-0-us-east-1.pooler.supabase.com:5432/postgres?sslmode=require";
     private static final String USER = "postgres.kbtpsxbckhpoqiqpgcji"; // Usuario del pooler
-    private static final String PASSWORD = "2c0tHKjUydH2Vglq"; // Contraseña del pooler
+    private static final String PASSWORD = "Capstone.13$78"; // Contraseña del pooler
 
     public static Connection getConnection() throws SQLException {
         System.setProperty("java.net.preferIPv4Stack", "true"); // Evita conflictos IPv6
